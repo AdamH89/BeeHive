5239c4fe208300074efe8ccd87f1fa5c08b8bfa3
AdamH89 <71062775+AdamH89@users.noreply.github.com>
2021-06-30 14:15:30 -0700

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BeeHive.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace BeeHive.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly MyContext _context;
+
+        public HomeController(MyContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("ingrid")]
+        public IActionResult Ingrid()
+        {
+            return View();
+        }
+
+        [HttpGet("kaytea")]
+        public IActionResult Kaytea()
+        {
+            return View();
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
